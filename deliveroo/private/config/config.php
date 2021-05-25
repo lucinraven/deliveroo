292c1c5994547c78f5c1bd694c2de2f04111d151
lucinraven <79331208+lucinraven@users.noreply.github.com>
2021-05-25 18:43:37 +0400

    Delete deliveroo directory

--- deliveroo/private/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$con = mysqli_connect("localhost", "root", "", "deliveroo"); // setting variable for database
-
-if($con->connect_error){
-    die("connection failed: ".$con->connect_error);
-}
-?>
