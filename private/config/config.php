31946561dbbb218f7ba136e22df951a7d66f132d
lucinraven <79331208+lucinraven@users.noreply.github.com>
2021-05-26 19:24:38 +0400

    Delete private directory

--- private/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start(); 
-
-$con = mysqli_connect("localhost", "root", "", "deliveroo"); // setting variable for database
-
-if($con->connect_error){
-    die("connection failed: ".$con->connect_error);
-}
-?>
